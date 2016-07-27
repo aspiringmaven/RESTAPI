dedabfe10f6077a055cd8dae9ba6b12b3c19e6f2
Sumit Kawatra <sumitkkawatra@gmail.com>
2016-07-27 23:32:08 +0530

    PUT/DELETE/POST User Stub added.

--- src/main/java/me/sumitkawatra/webservice/resource/UserResource.java
+++ src/main/java/me/sumitkawatra/webservice/resource/UserResource.java
@@ -4,11 +4,18 @@ package me.sumitkawatra.webservice.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -41,6 +48,31 @@ public class UserResource {
 		return new User();
 	}
 	
+	
+	@DELETE
+	@Path("/{userId}")
+	public Response deleteUser(@PathParam("userId") int userId) {
+		//TODO
+		return Response.status(Status.OK).build();
+	}
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/userId")
+	public User updateUser(@PathParam("userId") int userId, User user) {
+		//TODO
+		return user;
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/")
+	public User saveUser(User user) {
+		//TODO
+		return user;
+	}	
 
 	public UserService getUserService() {
 		return userService;
