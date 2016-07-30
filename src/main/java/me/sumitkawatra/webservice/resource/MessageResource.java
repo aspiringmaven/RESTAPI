097009cb26261164fd60769df0fe1de9784d02ba
Sumit Kawatra <sumitkkawatra@gmail.com>
2016-07-31 01:07:16 +0530

    Added get Message REST call

--- src/main/java/me/sumitkawatra/webservice/resource/MessageResource.java
+++ src/main/java/me/sumitkawatra/webservice/resource/MessageResource.java
@@ -1,10 +1,16 @@
 package me.sumitkawatra.webservice.resource;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import me.sumitkawatra.webservice.domain.Message;
 import me.sumitkawatra.webservice.service.MessageService;
 
 @Component
@@ -13,15 +19,23 @@ public class MessageResource {
 
 	@Autowired
 	private MessageService messageService;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages() {
+		return messageService.getAll();
+	}
+	
 	
 	public MessageResource() {
 		super();
 	}
-
+	
 	public MessageService getMessageService() {
 		return messageService;
 	}
-
+	
+	
 	public void setMessageService(MessageService messageService) {
 		this.messageService = messageService;
 	}
